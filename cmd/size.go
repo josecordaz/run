f84fadbf8b32f55a1ee2441c2f8942fa43748d46
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2017-12-21 15:57:13 -0600

    size description

--- cmd/size.go
+++ cmd/size.go
@@ -24,14 +24,9 @@ import (
 
 // sizeCmd represents the size command
 var sizeCmd = &cobra.Command{
-	Use:   "size",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "size [FOLDER]",
+	Short: "Prints FOLDER size",
+	Long:  `Checks folder size resursively`,
 	Run: func(cmd *cobra.Command, args []string) {
 		folder := args[0]
 		size := getFolderSize(folder)
