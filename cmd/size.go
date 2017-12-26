90ae64d67460bb7e036f9bb3aae491634eac2874
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2017-12-26 11:44:51 -0600

    run size to get all folders and files sizes

--- cmd/size.go
+++ cmd/size.go
@@ -17,10 +17,25 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+type Item struct {
+	name string
+	size float64
+}
+
+type BySize []Item
+
+func (a BySize) Len() int           { return len(a) }
+func (a BySize) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a BySize) Less(i, j int) bool { return a[i].size > a[j].size }
 
 // sizeCmd represents the size command
 var sizeCmd = &cobra.Command{
@@ -28,20 +43,65 @@ var sizeCmd = &cobra.Command{
 	Short: "Prints FOLDER size",
 	Long:  `Checks folder size resursively`,
 	Run: func(cmd *cobra.Command, args []string) {
-		folder := args[0]
-		size := getFolderSize(folder)
-		switch {
-		case size < 1024:
-			fmt.Println(size, "b")
-		case size < 1048576:
-			fmt.Printf("%.2f %s\n", size/1024, "K")
-		case size < 1073741824:
-			fmt.Printf("%.2f %s\n", size/1048576, "M")
-		case size < 1099511627776:
-			fmt.Printf("%.2f %s\n", size/1073741824, "G")
-
+		if len(args) == 0 {
+			items := make([]Item, 0)
+			dir, err := os.Getwd()
+			if err != nil {
+				log.Fatal(err)
+			}
+			files, err := ioutil.ReadDir(dir)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			for _, file := range files {
+				if file.IsDir() {
+					items = append(items, Item{file.Name(), getFolderSize(dir + "/" + file.Name())})
+				} else {
+					items = append(items, Item{file.Name(), float64(file.Size())})
+				}
+			}
+			longestName := len(items[0].name)
+			for item := range items {
+				if len(items[item].name) > longestName {
+					longestName = len(items[item].name)
+				}
+			}
+			sort.Sort(BySize(items))
+			for item := range items {
+				name := items[item].name
+				size := items[item].size
+				fmt.Printf("%s %s %s\n", name, strings.Repeat(" ", longestName-len(name)), getSize(size))
+			}
+		} else {
+			folder := args[0]
+			fmt.Println(checkFolderSize(folder))
 		}
 	},
+}
+
+func checkFolderSize(folder string) string {
+	size := getFolderSize(folder)
+	return getSize(size)
+}
+
+func getSize(size float64) string {
+	//strconv.FormatFloat(input_num, 'f', 6, 64)
+	switch {
+	case size < 1024:
+		s := strconv.FormatFloat(size, 'f', 0, 64)
+		return fmt.Sprint(s, strings.Repeat(" ", 6-len(s)), "b")
+	case size < 1048576:
+		s := strconv.FormatFloat(size/1024, 'f', 2, 64)
+		return fmt.Sprint(s, strings.Repeat(" ", 6-len(s)), "K")
+	case size < 1073741824:
+		s := strconv.FormatFloat(size/1048576, 'f', 2, 64)
+		return fmt.Sprint(s, strings.Repeat(" ", 6-len(s)), "M")
+	case size < 1099511627776:
+		s := strconv.FormatFloat(size/1073741824, 'f', 2, 64)
+		return fmt.Sprint(s, strings.Repeat(" ", 6-len(s)), "G")
+	}
+	return "-1"
 }
 
 func getFolderSize(folder string) float64 {
