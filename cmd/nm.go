b2ce076a885f2f78b62ed124013ddd19838b7303
Jose Carlos Ordaz <josecordaz@gmail.com>
2019-11-02 10:34:23 -0500

    clean up

--- cmd/nm.go
+++ cmd/nm.go
@@ -58,14 +58,4 @@ func checkDir(folder string) {
 
 func init() {
 	rootCmd.AddCommand(nmCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// nmCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// nmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
