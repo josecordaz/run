4e08183f6f7323774402f9fd8b1b971c228f3df8
J Carlos Ordaz <josecordaz@gmail.com>
2020-05-15 10:13:26 -0700

    test gpg key

--- cmd/datamodel.go
+++ cmd/datamodel.go
@@ -14,13 +14,7 @@ import (
 // datamodelCmd represents the datamodel command
 var datamodelCmd = &cobra.Command{
 	Use:   "datamodel",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "move files easier",
 	Run: func(cmd *cobra.Command, args []string) {
 		projectDest := "agent"
 		fileDest := "*"
